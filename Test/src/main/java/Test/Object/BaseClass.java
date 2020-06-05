1824d05d1670727c9418526f568c803882d33d18
phungle3001 <phungle3001@gmail.com>
2020-06-05 21:20:50 +0700

    my project

---
+++ Test/src/main/java/Test/Object/BaseClass.java
@@ -0,0 +1,14 @@
+package Test.Object;
+
+import org.openqa.selenium.WebDriver;
+public class BaseClass {
+	public static WebDriver driver;
+	public  BaseClass(WebDriver driver){
+		BaseClass.driver = driver;
+	}	
+	
+	public static void Delay (int ms) throws InterruptedException
+	{
+		Thread.sleep(ms);
+	}
+}
